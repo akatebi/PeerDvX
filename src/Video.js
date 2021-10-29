3006eed301257277564c8ea5fe3db4725c21def3
Alex Katebi <akatebi@optimusride.com>
2021-10-29 00:52:58 -0400

    f

--- src/Video.js
+++ src/Video.js
@@ -1,52 +1,65 @@
 import { openUserMedia, createRoom, joinRoom, hangUp } from "./WebRTC";
-import { useState, useRef } from "react"
+import { useState, useRef } from "react";
 
 const Video = () => {
-    const [mediaBtn, disableMediaBtn] = useState(false);
-    const [createBtn, disableCreateBtn] = useState(true);
-    const [joinBtn, disableJoinBtn] = useState(true);
-    const [hangupBtn, disableHangupBtn] = useState(true);
-    const [roomId, setRoomId] = useState(null);
-    const localVideo = useRef(null);
-    const remoteVideo = useRef(null);
+  const [mediaBtn, disableMediaBtn] = useState(false);
+  const [createBtn, disableCreateBtn] = useState(true);
+  const [joinBtn, disableJoinBtn] = useState(true);
+  const [hangupBtn, disableHangupBtn] = useState(true);
+  const [openDialog, setOpenDialog] = useState(false);
+  const [roomId, setRoomId] = useState(null);
+  const localVideo = useRef(null);
+  const remoteVideo = useRef(null);
 
   return (
     <div>
       <h1>Welcome to PeerDvX!</h1>
       <div id="buttons">
-        <button disabled={mediaBtn} onClick={ evt => {
+        <button
+          disabled={mediaBtn}
+          onClick={(evt) => {
             openUserMedia(localVideo, remoteVideo);
             disableMediaBtn(true);
             disableCreateBtn(false);
             disableJoinBtn(false);
-        }}>
+          }}
+        >
           <span>Open camera & microphone</span>
         </button>
-        <button disabled={createBtn} onClick={ async evt => {
-          disableCreateBtn(true);
-          disableJoinBtn(true);
-          disableHangupBtn(false);
-          setRoomId(await createRoom(localVideo));
-        }}>
+        <button
+          disabled={createBtn}
+          onClick={async (evt) => {
+            disableCreateBtn(true);
+            disableJoinBtn(true);
+            disableHangupBtn(false);
+            setRoomId(await createRoom(localVideo));
+          }}
+        >
           <span>Create room</span>
         </button>
-        <button disabled={joinBtn}>
+        <button disabled={joinBtn} onClick={async (evt) => {
+          setOpenDialog(true);
+        }}>
           <span>Join room</span>
         </button>
-        <button disabled={hangupBtn} onClick={
-          async evt => {
+        <button
+          disabled={hangupBtn}
+          onClick={async (evt) => {
             await hangUp(localVideo, remoteVideo, roomId);
             setRoomId(null);
             disableMediaBtn(false);
             disableCreateBtn(true);
             disableJoinBtn(true);
             disableHangupBtn(true);
-        }}>
+          }}
+        >
           <span>Hangup</span>
         </button>
       </div>
       <div>
-        {roomId && <span>{`Current room is ${roomId} - You are the caller!`}</span>}
+        {roomId && (
+          <span>{`Current room is ${roomId} - You are the caller!`}</span>
+        )}
       </div>
       <div id="videos">
         <video ref={localVideo} muted autoPlay playsInline></video>
@@ -59,31 +72,29 @@ const Video = () => {
         aria-labelledby="my-dialog-title"
         aria-describedby="my-dialog-content"
       >
-        <div>
+        <dialog open={openDialog}>
+          <h2>Join room</h2>
           <div>
-            <h2 id="my-dialog-title">Join room</h2>
-            <div id="my-dialog-content">
-              Enter ID for room to join:
-              <div>
-                <input type="text" id="room-id" />
-                <label htmlFor="my-text-field">Room ID</label>
-                <div></div>
-              </div>
+            Enter ID for room to join:
+            <div>
+              <input type="text" id="room-id" />
+              <label htmlFor="my-text-field">Room ID</label>
+              <div></div>
             </div>
-            <footer>
-              <button type="button" data-mdc-dialog-action="no">
-                <span>Cancel</span>
-              </button>
-              <button
-                id="confirmJoinBtn"
-                type="button"
-                data-mdc-dialog-action="yes"
-              >
-                <span>Join</span>
-              </button>
-            </footer>
           </div>
-        </div>
+          <footer>
+            <button onClick={evt => setOpenDialog(false)}>
+              <span>Cancel</span>
+            </button>
+            <button
+              id="confirmJoinBtn"
+              type="button"
+              data-mdc-dialog-action="yes"
+            >
+              <span>Join</span>
+            </button>
+          </footer>
+        </dialog>
       </div>
     </div>
   );
